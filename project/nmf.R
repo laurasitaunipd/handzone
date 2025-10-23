b4ae4b5e0bb6d83388fb3f48849a7d1b92dc18bf
laurasitaunipd <laura.sita.2000@gmail.com>
2025-10-23 13:06:03 +0200

    update

--- project/nmf.R
+++ project/nmf.R
@@ -4,6 +4,8 @@
   #install.packages("BiocManager")
 #BiocManager::install("Biobase")
 #remotes::install_github("jennalandy/causalLFO")
+#install.packages("lsa")
+#install.packages("RcppHungarian")
 
 
 # pacchetti
@@ -12,6 +14,7 @@ library(causalLFO)
 library(tidyverse)
 library(ggridges)
 library(ggplot2)
+# in caso aggiungi anche le ultime due packages come library se dà errore in B
 
 
 ############################# APPLICAZIONE SU 1 DATASET SIMULATO CON N=100
@@ -68,11 +71,106 @@ for (i in 1:N) {
   M[,i] <- rpois(D, lambda = true_P %*% true_C[,i])
 }
 
-#View(M)
-
 # B) STIME DI TRUE_P(LAMBDA) E TRUE_C(L)
 
+all_data_fit <- all_data(M, Tr, rank = 3, reference_P = true_P)
+impute_and_stabilize_fit <- impute_and_stabilize(M, Tr, rank = 3, reference_P = true_P)
 
+# Fattori latenti stimati
+L_all <- all_data_fit$L_hat
+L_IS  <- impute_and_stabilize_fit$L_hat
+
+# visualizzo Chat(Lhat)
+impute_and_stabilize_fit$Chat
+class(impute_and_stabilize_fit$Chat)
+dim(impute_and_stabilize_fit$Chat) # 3 fattori x 100 sogg
+
+# SOLO PER K1 CONFRONTO ALGORITMI CON ANCHE C-VERI
+# Estrai i fattori stimati (le Chat)
+Chat_all <- all_data_fit$Chat
+Chat_IS  <- impute_and_stabilize_fit$Chat
+
+# ---- Funzione per creare il plot per un dato fattore k ----
+plot_latent_factor <- function(k, true_C, all_data_fit, impute_and_stabilize_fit) {
+  
+  df_plot <- data.frame(
+    True = rep(true_C[k,], 2),
+    Estimated = c(all_data_fit$Chat[k,], impute_and_stabilize_fit$Chat[k,]),
+    Method = rep(c("All Data", "Impute & Stabilize"), each = length(true_C[k,]))
+  )
+  
+  ggplot(df_plot, aes(x = True, y = Estimated, color = Method)) +
+    geom_point(alpha = 0.6, size = 2) +                         # punti semitrasparenti
+    geom_abline(intercept = 0, slope = 1, color = "red", linewidth = 1) +  # linea 1:1
+    theme_bw() +
+    theme(
+      panel.grid = element_blank(),
+      text = element_text(size = 13),
+      legend.position = "top",
+      legend.title = element_blank()
+    ) +
+    scale_color_manual(
+      values = c("All Data" = "blue", "Impute & Stabilize" = "darkorange")
+    ) +
+    labs(
+      x = paste("True latent factor", k),
+      y = paste("Estimated latent factor", k),
+      title = paste("Comparison of latent factor", k)
+    )
+}
+
+# Fattore 1
+plot_latent_factor(1, true_C, all_data_fit, impute_and_stabilize_fit)
+
+# Fattore 2
+plot_latent_factor(2, true_C, all_data_fit, impute_and_stabilize_fit)
+
+# Fattore 3
+plot_latent_factor(3, true_C, all_data_fit, impute_and_stabilize_fit)
+
+
+# C) ATE E CONFRONTO DEI DUE ALGORITMI 
+impute_and_stabilize_res <- impute_and_stabilize(
+  M, Tr, rank = 3, reference_P = true_P
+)
+class(impute_and_stabilize_res)
+summary(impute_and_stabilize_res)
+plot(impute_and_stabilize_res)
+
+all_data_res <- all_data(
+  M, Tr, rank = 3, reference_P = true_P)
+summary(all_data_res)
+
+res_list <- list(
+  'All Data' = all_data_res,
+  'Impute and Stabilize' = impute_and_stabilize_res
+)
+plot_causalLFO_results(res_list)
+
+####################################################### PROVA SENZA REFERENCE_P
+impute_and_stabilize_res_noP <- impute_and_stabilize(
+  M, Tr, rank = 3)
+summary(impute_and_stabilize_res_noP)
+
+all_data_res_noP <- all_data(
+  M, Tr, rank = 3)
+summary(all_data_res_noP)
+
+res_list <- list(
+  'All Data' = all_data_res,
+  'All Data noP' = all_data_res_noP,
+  'Impute and Stabilize' = impute_and_stabilize_res,
+  'Impute and Stabilize noP' = impute_and_stabilize_res_noP
+)
+
+plot_causalLFO_results(res_list) + scale_color_manual(
+  values = c(
+    "All Data"                 = "#e34a33",  
+    "All Data noP"             = "#fdbb84",  
+    "Impute and Stabilize"     = "#0570b0",  
+    "Impute and Stabilize noP" = "#74a9cf"  
+  )
+)
 
 
 
@@ -81,5 +179,5 @@ for (i in 1:N) {
 ############################# APPLICAZIONE SU 100 DATASET SIMULATI (usando le funzioni in simulate.r)
 
 source("/Users/laura/Desktop/GITHUB/handzone/project/simulate.R")
-
+# in caso metti un percorso assoluto che apra il file dalla cartella
 
